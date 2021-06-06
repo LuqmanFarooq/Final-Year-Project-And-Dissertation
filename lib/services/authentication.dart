88d42a3ae6bafba8764786a557c7d0053592e1e4
LuqmanFarooq <g00353385@gmit.ie>
2021-06-06 01:08:08 +0100

    Authentication Methods added

--- lib/services/authentication.dart
+++ lib/services/authentication.dart
@@ -1,5 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class authentication with ChangeNotifier {}
+class authentication with ChangeNotifier {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  String userUid;
+  String get getUserid => userUid;
+
+  Future logIntoAccount(String email, String password) async {
+    UserCredential userCredential = await firebaseAuth
+        .signInWithEmailAndPassword(email: email, password: password);
+
+    User user = userCredential.user;
+    userUid = user.uid;
+    print(userUid);
+    notifyListeners();
+  }
+
+  Future createAccount(String email, String password) async {
+    UserCredential userCredential = await firebaseAuth
+        .createUserWithEmailAndPassword(email: email, password: password);
+
+    User user = userCredential.user;
+    userUid = user.uid;
+    print(userUid);
+    notifyListeners();
+  }
+
+  Future signInWithGoogle() async {
+    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
+    final AuthCredential authCredential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken);
+    final UserCredential userCredential =
+        await firebaseAuth.signInWithCredential(authCredential);
+    final User user = userCredential.user;
+    assert(user.uid != null);
+
+    userUid = user.uid;
+    print('Google User uid => $userUid');
+    notifyListeners();
+  }
+}
