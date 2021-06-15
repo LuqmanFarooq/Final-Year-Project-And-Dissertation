a2585060beb9d82bfb7dfef2f24725fa0091a0a2
LuqmanFarooq <g00353385@gmit.ie>
2021-06-16 00:13:26 +0100

    working on homepage

--- lib/screens/Homepage/homepage.dart
+++ lib/screens/Homepage/homepage.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:the_social/constants/Constantcolors.dart';
 
-class homepage extends StatelessWidget {
+class homepage extends StatefulWidget {
+  @override
+  _homepageState createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
   ConstantColors constantColors = ConstantColors();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: constantColors.redColor,
-    );
+    return Scaffold(backgroundColor: constantColors.darkColor);
   }
 }
