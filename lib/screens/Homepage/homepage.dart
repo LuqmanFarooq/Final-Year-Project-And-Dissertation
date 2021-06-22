ed9f3df8488f0f7e1660a04bb48f4a5fb9fdf679
Muhammad Noman <g00351754@gmit.ie>
2021-06-22 14:02:37 +0100

    working on custom bottom Navigation

--- lib/screens/Homepage/homepage.dart
+++ lib/screens/Homepage/homepage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:the_social/constants/Constantcolors.dart';
+import 'package:the_social/screens/Chatroom/chatroom.dart';
+import 'package:the_social/screens/Feed/feed.dart';
+import 'package:the_social/screens/Profile/profile.dart';
 
 class homepage extends StatefulWidget {
   @override
@@ -9,14 +12,20 @@ class homepage extends StatefulWidget {
 class _homepageState extends State<homepage> {
   ConstantColors constantColors = ConstantColors();
   final PageController homepageController = PageController();
-
+  int pageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: constantColors.darkColor,
       body: PageView(
         controller: homepageController,
-        children: [],
+        children: [feed(), chatroom(), profile()],
+        physics: NeverScrollableScrollPhysics(),
+        onPageChanged: (page) {
+          setState(() {
+            pageIndex = page;
+          });
+        },
       ),
     );
   }
